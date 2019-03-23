f0d7472fbca8e2a5e60208a0201b1d5f172c3295
Ted Naleid <contact@naleid.com>
2019-03-23 12:35:34 -0500

    close

--- stitch.py
+++ stitch.py
@@ -17,20 +17,19 @@ from PIL import Image
 result = Image.new("LA", (3584, 4605))
 
 # all images are 256x256 except for the last row which is only 253 wide
-for i in range(14):
-    for j in range(18):
-        file = './raw/{0}_{1}.jpg'.format(i, j)
-        width = 256 if j < 17 else 253
+for x_index in range(18):
+    for y_index in range(14):
+        file = './raw/{0}_{1}.jpg'.format(y_index, x_index)
+        width = 256 if x_index < 17 else 253
         height = 256
         print('{0} {1}x{2}'.format(file, width, height))
 
         path = os.path.expanduser(file)
         img = Image.open(path)
         img.thumbnail((width, height), Image.ANTIALIAS)
-        x = i * 256
-        y = j * 256
+        x = x_index * 256
+        y = y_index * 256
         print('pos {0},{1} size {2},{3}'.format(x, y, width, height))
-        result.paste(img,  (x, y, x + width, y + height))
+        result.paste(img, (x, y, x + width, y + height))
         converted_result = result.convert('RGB')
         converted_result.save(os.path.expanduser('./output.jpg'))
-
