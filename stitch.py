87e96c414a721544c5539d0ced381561a93f7ece
Ted Naleid <contact@naleid.com>
2019-03-23 12:27:15 -0500

    almost working

--- stitch.py
+++ stitch.py
@@ -14,22 +14,23 @@ import os
 
 from PIL import Image
 
-files = [
-  '~/Downloads/1.jpg',
-  '~/Downloads/2.jpg',
-  '~/Downloads/3.jpg',
-  '~/Downloads/4.jpg']
+result = Image.new("LA", (3584, 4605))
 
-result = Image.new("RGB", (800, 800))
+# all images are 256x256 except for the last row which is only 253 wide
+for i in range(14):
+    for j in range(18):
+        file = './raw/{0}_{1}.jpg'.format(i, j)
+        width = 256 if j < 17 else 253
+        height = 256
+        print('{0} {1}x{2}'.format(file, width, height))
 
-for index, file in enumerate(files):
-  path = os.path.expanduser(file)
-  img = Image.open(path)
-  img.thumbnail((400, 400), Image.ANTIALIAS)
-  x = index // 2 * 400
-  y = index % 2 * 400
-  w, h = img.size
-  print('pos {0},{1} size {2},{3}'.format(x, y, w, h))
-  result.paste(img, (x, y, x + w, y + h))
+        path = os.path.expanduser(file)
+        img = Image.open(path)
+        img.thumbnail((width, height), Image.ANTIALIAS)
+        x = i * 256
+        y = j * 256
+        print('pos {0},{1} size {2},{3}'.format(x, y, width, height))
+        result.paste(img,  (x, y, x + width, y + height))
+        converted_result = result.convert('RGB')
+        converted_result.save(os.path.expanduser('./output.jpg'))
 
-result.save(os.path.expanduser('~/image.jpg'))
